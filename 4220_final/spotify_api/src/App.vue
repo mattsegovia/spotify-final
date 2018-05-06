708bab3f488a38c64e5b2bfd79aab4998e977f92
asolon11 <aly.solon11@gmail.com>
2018-05-06 00:44:58 -0700

    added a short description

--- 4220_final/spotify_api/src/App.vue
+++ 4220_final/spotify_api/src/App.vue
@@ -2,63 +2,50 @@
   <div id="app">
 
   <nav class="navbar is-transparent is-dark">
-              <div class="navbar-brand">
-                <a class="navbar-item" href="https://www.spotify.com/us/" w>
-                  <img src="./assets/spotify_logo_green.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-                </a>
-                <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-              </div>
+      <div class="navbar-brand">
+          <a class="navbar-item" href="https://www.spotify.com/us/" w>
+            <img src="./assets/spotify_logo_green.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+          </a>
+          <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+      </div>
             
-              <div id="navbarExampleTransparentExample" class="navbar-menu">
-                <div class="navbar-start">
-                  <a class="navbar-item" href="https://bulma.io/">
-                    Home
-                  </a>
-                  <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link" href="/documentation/overview/start/">
-                      Docs
-                    </a>
-                    <div class="navbar-dropdown is-boxed">
-                      <a class="navbar-item" href="/documentation/overview/start/">
-                        Overview
-                      </a>
-                      <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
-                        Modifiers
-                      </a>
-                      <hr class="navbar-divider">
-                      <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                        Elements
-                      </a>
-                      <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                        Components
-                      </a>
-                    </div>
-                  </div>
-                </div>
-            
-                <div class="navbar-end">
-                  <div class="navbar-item">
-                    <div class="field is-grouped">
-                      <p class="control">
-                      
-                    
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </nav>
+      <div id="navbarExampleTransparentExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="https://bulma.io/">
+            Home
+          </a>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link" href="/documentation/overview/start/">
+              Docs
+            </a>
+            <div class="navbar-dropdown is-boxed">
+              <a class="navbar-item" href="/documentation/overview/start/">
+                Overview
+              </a>
+              <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
+                Modifiers
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
+                Elements
+              </a>
+              <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
+                Components
+              </a>
+            </div>
+          </div>
+        </div>
+             
+      </div>
+    </nav>
 
-          <search-component/> <!-- SEARCH BAR *********************** -->
-                      
-
-
-    <!-- <img src="./assets/logo.png"> -->
-    <h1>{{ msg }}</h1>
+    <br />
+    <h1 class="title">{{ msg }}</h1>
+    <search-component /> <!-- SEARCH BAR *********************** -->
 
   </div>
 </template> 
@@ -71,7 +58,7 @@ export default {
   name: "app",
   data() {
     return {
-      msg: "Search Artist Information"
+      msg: "Search for an artist, song, or album!"
     };
   },
   components: {
@@ -92,11 +79,6 @@ export default {
   /* margin-top: 60px; */
 }
 
-h1,
-h2 {
-  font-weight: normal;
-}
-
 ul {
   list-style-type: none;
   padding: 0;
@@ -111,7 +93,4 @@ a {
   color: #42b983;
 }
 
-/* .navbar{
-  background-color: black;
-} */
 </style>
